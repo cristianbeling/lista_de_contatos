639f16eaa34b80315dde672fe2fcb78101b88536
cristianbeling <cristianbeling@hotmail.com>
2023-10-05 16:41:16 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form-atividade');
+const contato = [];
+const numero = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+    atualizanotafinal();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    if (contato.includes(inputNomeContato.value)) {
+        alert(`A atividade: ${inputNomeContato.value} já foi inserida`);
+    } else {
+        contato.push(inputNomeContato.value);
+        numero.push(parseFloat(inputNumeroContato.value));
+    
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputNumeroContato.value}</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;
+    }
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
